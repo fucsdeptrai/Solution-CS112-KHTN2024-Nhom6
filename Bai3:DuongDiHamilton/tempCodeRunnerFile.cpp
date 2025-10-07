fb38ef90d5abf1d366f6686cf2d18a04d6d75a3a
NguyenHongPhuc <138000410+fucsdeptrai@users.noreply.github.com>
2025-10-07 20:28:51 +0700

    Delete Bai3:DuongDiHamilton/tempCodeRunnerFile.cpp

--- Bai3:DuongDiHamilton/tempCodeRunnerFile.cpp
+++
@@ -1,7 +0,0 @@
-rmutation(1, n);
-    // for (int i = 1; i <= n; i++) {
-    //     for (int j = 0; j < (int)adj[i].size(); j++) {
-    //         if(adj[i][j] < i) continue;
-    //         cout << label_map[i] << " " << label_map[adj[i][j]] << "\n";
-    //     }
-    // }
